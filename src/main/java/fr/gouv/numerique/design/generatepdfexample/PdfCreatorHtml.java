1bc82f9338f3b970c0cab03a020f9972bbfe4666
Yaacov <yaacov@goodimpact.studio>
2023-10-27 18:46:00 +0200

    ♿️ Feat(font): embed Marianne font (7 versions)
    PDF generated from HTML (HTML_GOOD) is now fully accessible!

--- src/main/java/fr/gouv/numerique/design/generatepdfexample/PdfCreatorHtml.java
+++ src/main/java/fr/gouv/numerique/design/generatepdfexample/PdfCreatorHtml.java
@@ -2,6 +2,10 @@ package fr.gouv.numerique.design.generatepdfexample;
 
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.html2pdf.resolver.font.DefaultFontProvider;
+import com.itextpdf.io.font.*;
+import com.itextpdf.kernel.pdf.*;
+import com.itextpdf.layout.font.*;
 import fr.gouv.numerique.design.generatepdfexample.FormData;
 import freemarker.template.*;
 import java.io.*;
@@ -12,6 +16,14 @@ import java.util.*;
  *
  */
 public class PdfCreatorHtml extends PdfCreator {
+
+  public static final String FONT_LIGHT = "src/main/resources/fonts/Marianne-Light.woff2";
+  public static final String FONT_LIGHT_ITALIC = "src/main/resources/fonts/Marianne-Light_Italic.woff2";
+  public static final String FONT_REGULAR = "src/main/resources/fonts/Marianne-Regular.woff2";
+  public static final String FONT_REGULAR_ITALIC = "src/main/resources/fonts/Marianne-Regular_Italic.woff2";
+  public static final String FONT_MEDIUM = "src/main/resources/fonts/Marianne-Medium.woff2";
+  public static final String FONT_MEDIUM_ITALIC = "src/main/resources/fonts/Marianne-Medium_Italic.woff2";
+  public static final String FONT_BOLD = "src/main/resources/fonts/Marianne-Bold.woff2";
 
   public PdfCreatorHtml() throws IOException {
     super();
@@ -40,12 +52,36 @@ public class PdfCreatorHtml extends PdfCreator {
     FileOutputStream fout = new FileOutputStream(destHtml);
     fout.write(htmlStr.getBytes());
 
-    ConverterProperties props = new ConverterProperties();
-    props.setBaseUri("./src/main/resources/");
-
     // Destination. Ex: "./dist/formation_creer_un_pdf_accessible_recapitulatif__HTML_BAD.pdf"
     String dest = DEST_PATH + DEST_BASE_FILE_NAME + "__HTML_" + suffix + "." + DEST_EXTENSTION;
+    fout = new FileOutputStream(dest);
 
-    HtmlConverter.convertToPdf(htmlStr, new FileOutputStream(dest), props);
+    // ACCESSIBILITY
+    WriterProperties writerProperties = new WriterProperties();
+    // writerProperties.addXmpMetadata();
+    writerProperties.addUAXmpMetadata().setPdfVersion(PdfVersion.PDF_2_0);
+
+    PdfWriter pdfWriter = new PdfWriter(fout, writerProperties);
+    PdfDocument pdfDoc = new PdfDocument(pdfWriter);
+    pdfDoc.getCatalog().setLang(new PdfString("fr-FR"));
+
+    pdfDoc.setTagged();
+    pdfDoc.getCatalog().setViewerPreferences(new PdfViewerPreferences().setDisplayDocTitle(true));
+
+    // Embed font
+    ConverterProperties props = new ConverterProperties();
+    FontProvider fontProvider = new DefaultFontProvider(true, false, false);
+    fontProvider.addFont(FontProgramFactory.createFont(FONT_LIGHT), "UTF-8");
+    fontProvider.addFont(FontProgramFactory.createFont(FONT_LIGHT_ITALIC), "UTF-8");
+    fontProvider.addFont(FontProgramFactory.createFont(FONT_REGULAR), "UTF-8");
+    fontProvider.addFont(FontProgramFactory.createFont(FONT_REGULAR_ITALIC), "UTF-8");
+    fontProvider.addFont(FontProgramFactory.createFont(FONT_MEDIUM), "UTF-8");
+    fontProvider.addFont(FontProgramFactory.createFont(FONT_MEDIUM_ITALIC), "UTF-8");
+    fontProvider.addFont(FontProgramFactory.createFont(FONT_BOLD), "UTF-8");
+    props.setFontProvider(fontProvider);
+
+    props.setBaseUri("./src/main/resources/");
+
+    HtmlConverter.convertToPdf(htmlStr, pdfDoc, props);
   }
 }
